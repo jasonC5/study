d76cad38d4d67ab65e48b5d8abaa2474a5c0dfa8
JasonC5 <chenj@migrsoft.com>
2021-06-30 19:21:35 +0800

    K算法、P算法

--- src/com/jason/study/algorithm/graph/Kruskal.java
+++ src/com/jason/study/algorithm/graph/Kruskal.java
@@ -1,5 +1,7 @@
 package com.jason.study.algorithm.graph;
 
+import java.util.*;
+
 /**
  * K算法--最小生成树
  * 1）总是从权值最小的边开始考虑，依次考察权值依次变大的边
@@ -10,4 +12,84 @@ package com.jason.study.algorithm.graph;
  * @author JasonC5
  */
 public class Kruskal {
+
+    static class UnionFind{
+        // key 某一个节点， value key节点往上的节点
+        private HashMap<Node, Node> fatherMap;
+        // key 某一个集合的代表节点, value key所在集合的节点个数
+        private HashMap<Node, Integer> sizeMap;
+
+        public UnionFind() {
+            this.fatherMap = new HashMap<>();
+            this.sizeMap = new HashMap<>();
+        }
+
+        public void makeSets(Collection<Node<Integer>> values) {
+            this.fatherMap = new HashMap<>();
+            this.sizeMap = new HashMap<>();
+            for (Node<Integer> value : values) {
+                fatherMap.put(value, value);
+                sizeMap.put(value, 1);
+            }
+        }
+
+        //查看是否在一个集合中，若在则形成环，
+        public boolean isSameSet(Node n1, Node n2) {
+            //代表节点相同则在一个集合中
+            return find(n1) == find(n2);
+        }
+
+        private Node find(Node node) {
+//            Node father;
+//            return (father = fatherMap.get(node)) == node ? node : find(father);
+            Stack<Node> path = new Stack<>();
+            Node father;
+            while (node != (father = fatherMap.get(node))){
+                path.add(node);
+                node = father;
+            }
+            //压缩路径
+            while (!path.isEmpty()){
+                Node pop = path.pop();
+                fatherMap.put(pop, father);
+            }
+            return father;
+        }
+
+        public void union(Node n1, Node n2) {
+            Node fatherA,fatherB;
+            //本身就在一个集合内，不合并
+            if ((fatherA = find(n1)) == (fatherB = find(n2))) {
+                return;
+            }
+            //小的合入大的中
+            Integer size1 = sizeMap.get(fatherA);
+            Integer size2 = sizeMap.get(fatherB);
+            //谁大谁当爹
+            Node father = size1 >= size2 ? fatherA : fatherB;
+            Node son = father == fatherA ? fatherB : fatherA;
+            sizeMap.put(father, size1 + size2);
+            sizeMap.remove(son);
+            fatherMap.put(son, father);
+        }
+    }
+
+    public static Set<Edge> kruskalMST(Graph<Integer> graph) {
+        UnionFind unionFind = new UnionFind();
+        unionFind.makeSets(graph.nodes.values());
+        // 从小的边到大的边，依次弹出，小根堆！
+        PriorityQueue<Edge> minHeap = new PriorityQueue<Edge>((e1, e2)->e1.weight - e2.weight);
+        for (Edge edge : graph.edges) {
+            minHeap.offer(edge);
+        }
+        Set<Edge> result = new HashSet<>();
+        while (!minHeap.isEmpty()) {
+            Edge edge = minHeap.poll();
+            if (!unionFind.isSameSet(edge.source, edge.target)) {
+                result.add(edge);
+                unionFind.union(edge.source, edge.target);
+            }
+        }
+        return result;
+    }
 }
--- src/com/jason/study/algorithm/graph/Prim.java
+++ src/com/jason/study/algorithm/graph/Prim.java
@@ -1,5 +1,9 @@
 package com.jason.study.algorithm.graph;
 
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
+
 /**
  * P算法--最小生成树
  * 1）可以从任意节点出发来寻找最小生成树
@@ -8,7 +12,55 @@ package com.jason.study.algorithm.graph;
  * 4）如果会，不要当前边，继续考察剩下解锁的边中最小的边，重复3）
  * 5）如果不会，要当前边，将该边的指向点加入到被选取的点中，重复2）
  * 6）当所有点都被选取，最小生成树就得到了
+ *
  * @author JasonC5
  */
 public class Prim {
+
+    public static Set<Edge> prim(Graph<Integer> graph) {
+        //解锁的点
+        HashSet<Node> nodeSet = new HashSet<>();
+        //解锁的边
+        Set<Edge> result = new HashSet<>(); // 依次挑选的的边在result里
+        //小根堆放边，每次弹出一条最小边
+        PriorityQueue<Edge> minHeap = new PriorityQueue<Edge>((e1, e2) -> e1.weight - e2.weight);
+        for (Node<Integer> node : graph.nodes.values()) { // 随便挑了一个点
+            //随便找一个点开始
+            if (nodeSet.contains(node)) {
+                //已经解锁过该节点了，不处理
+                continue;
+            }
+            nodeSet.add(node);
+            //边放入小跟堆
+            for (Edge edge : node.edges) {
+                minHeap.add(edge);
+            }
+            while (!minHeap.isEmpty()) {
+                Edge edge = minHeap.poll();
+                Node<Integer> target = edge.target;
+                if (!nodeSet.contains(target)){
+                    result.add(edge);
+                    nodeSet.add(target);
+                    if (nodeSet.size() == graph.nodes.size()) {
+                        //所有点都解锁了，最小生成树达成
+                        return result;
+                    }
+                    for (Edge next : target.edges) {
+                        minHeap.add(next);
+                    }
+                }
+            }
+            //若可保证无森林，则直接break即可
+//            break;
+        }
+        return result;
+    }
+
+    // 请保证graph是连通图
+    // graph[i][j]表示点i到点j的距离，如果是系统最大值代表无路
+    // 返回值是最小连通图的路径之和
+    public static int prim(int[][] graph) {
+
+        return 0;
+    }
 }
